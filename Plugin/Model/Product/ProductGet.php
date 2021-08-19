b40a3385c9522f89cea92e81a4cdd2668c64f948
Irfan Ullah <eerfanmarwat@gmail.com>
2021-08-19 21:02:02 +0500

    Added extension_attributes to child products
    Added extension_attributes to configurable child products which will display current, quantity and product image in API response

--- Plugin/Model/Product/ProductGet.php
+++ Plugin/Model/Product/ProductGet.php
@@ -22,6 +22,20 @@ class ProductGet{
     protected $_imageHelper;
 
     /**
+    * @var \Magento\Catalog\Api\ProductRepositoryInterface
+    */
+    protected $productRepository;
+
+    /**
+    * @var \Magento\Catalog\Api\Data\ProductInterfaceFactory
+    */
+    protected $productFactory;
+
+    /**
+    * @var \Magento\Framework\Api\DataObjectHelper
+    */
+    protected $dataObjectHelper;
+    /**
     * @param \Magento\CatalogInventory\Api\StockStateInterface $stockItem
     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
     * @param \Magento\Catalog\Helper\Image $imageHelper
@@ -29,12 +43,18 @@ class ProductGet{
     public function __construct(
         \Magento\CatalogInventory\Api\StockStateInterface $stockItem,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Catalog\Helper\Image $imageHelper
+        \Magento\Catalog\Helper\Image $imageHelper,
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        \Magento\Catalog\Api\Data\ProductInterfaceFactory $productFactory,
+        \Magento\Framework\Api\DataObjectHelper $dataObjectHelper
     )
     {
         $this->_storeManager = $storeManager;
         $this->_stockItem = $stockItem;
         $this->_imageHelper = $imageHelper;
+        $this->productRepository = $productRepository;
+        $this->productFactory = $productFactory;
+        $this->dataObjectHelper = $dataObjectHelper;
     }
 
     /**
@@ -86,5 +106,57 @@ class ProductGet{
             $product->setExtensionAttributes($extensionattributes);
         }
         return $products;
+    }
+
+    public function getChildren($sku)
+    {
+        $currency = $this->_storeManager->getStore()->getCurrentCurrency()->getCode();
+        $product = $this->productRepository->get($sku);
+        if ($product->getTypeId() != \Magento\ConfigurableProduct\Model\Product\Type\Configurable::TYPE_CODE) {
+            return [];
+        }
+
+        $productTypeInstance = $product->getTypeInstance();
+        $productTypeInstance->setStoreFilter($product->getStoreId(), $product);
+
+        $childrenList = [];
+
+        foreach ($productTypeInstance->getUsedProducts($product) as $child) {
+            $attributes = [];
+            foreach ($child->getAttributes() as $attribute) {
+                $attrCode = $attribute->getAttributeCode();
+                $value = $child->getDataUsingMethod($attrCode) ?: $child->getData($attrCode);
+                if (null !== $value) {
+                    $attributes[$attrCode] = $value;
+                }
+            }
+            $attributes['store_id'] = $child->getStoreId();
+
+            $productDataObject = $this->productFactory->create();
+            $this->dataObjectHelper->populateWithArray(
+                $productDataObject,
+                $attributes,
+                \Magento\Catalog\Api\Data\ProductInterface::class
+            );
+
+            $qty = $this->_stockItem->getStockQty($productDataObject->getId(), $productDataObject->getStore()->getWebsiteId());
+
+
+            $extensionattributes = $productDataObject->getExtensionAttributes();
+
+            $extensionattributes->setQty((int)$qty);
+            
+            $price = $productDataObject->getPriceInfo()->getPrice('final_price')->getAmount()->getValue();
+            $extensionattributes->setAppboxoprice($price);
+            $extensionattributes->setAppboxocurrency($currency);
+            if($product->getImage()){
+                $extensionattributes->setAppboxoimage($this->_imageHelper->init($productDataObject, 'product_thumbnail_image')->setImageFile($productDataObject->getImage())->getUrl());
+            }
+            $productDataObject->setExtensionAttributes($extensionattributes);
+
+            $childrenList[] = $productDataObject;
+        }
+
+        return $childrenList;
     }
 }
