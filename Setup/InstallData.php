5866d57a90e90f1d631ec05458ecd29e794d1478
Irfan Ullah <eerfanmarwat@gmail.com>
2022-02-13 22:07:49 +0500

    Added auto create website and store functionlity

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -10,44 +10,69 @@ use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\DB\Ddl\Table;
 use Magento\Store\Model\StoreFactory;
-use Magento\Store\Model\StoreManagerInterface;
 use Magento\Framework\Event\ManagerInterface;
 use Magento\Store\Model\ResourceModel\Store;
+use Magento\Store\Model\WebsiteFactory;
+use Magento\Store\Model\ResourceModel\Website;
+use Magento\Store\Model\GroupFactory;
+use Magento\Store\Model\ResourceModel\Group;
 
 class InstallData implements InstallDataInterface
 {
+    /**
+     * @var WebsiteFactory
+     */
+    protected $_websiteFactory;
+    /**
+     * @var GroupFactory
+     */
+    protected $_groupFactory;
+    /**
+     * @var Group
+     */
+    protected $_groupResourceModel;
     /**
      * @var StoreFactory
      */
     protected $_storeFactory;
     /**
-     * @var StoreManagerInterface
-     */
-    protected $_storeManager;
-    /**
      * @var ManagerInterface
      */
     protected $_eventManager;
+     /**
+     * @var Website
+     */
+    protected $_websiteResourceModel;
     /**
      * @var Store
      */
     protected $_storeResourceModel;
-
     /**
-     * InstallData constructor.
-     * @param StoreFactory $storeFactory
-     * @param StoreManagerInterface $storeManager
+     * UpgradeSchema constructor.
+     * @param WebsiteFactory $WebsiteFactory
+     * @param Website $websiteResourceModel
+     * @param StoreFactory $StoreFactory
+     * @param ManagerInterface $eventManager
+     * @param GroupFactory $GroupFactory
+     * @param Group $groupResourceModel
+     * @param Store $storeResourceModel
      */
     public function __construct(
+        WebsiteFactory $websiteFactory,
+        Website $websiteResourceModel,
         StoreFactory $storeFactory,
-        StoreManagerInterface $storeManager,
         ManagerInterface $eventManager,
+        GroupFactory $groupFactory,
+        Group $groupResourceModel,
         Store $storeResourceModel
     ) {
+        $this->_websiteFactory = $websiteFactory;
+        $this->_websiteResourceModel = $websiteResourceModel;
         $this->_storeFactory = $storeFactory;
-        $this->_storeManager = $storeManager;
         $this->_eventManager = $eventManager;
         $this->_storeResourceModel = $storeResourceModel;
+        $this->_groupFactory = $groupFactory;
+        $this->_groupResourceModel = $groupResourceModel;
     }
 
     /**
@@ -103,13 +128,31 @@ class InstallData implements InstallDataInterface
                 ]
             );
 
+        /** @var \Magento\Store\Model\Website $website */
+        $website = $this->_websiteFactory->create();
+        $website->load('mp_web');
+        if(!$website->getId()){
+            $website->setCode('mp_web');
+            $website->setName('MP Website');
+            $this->_websiteResourceModel->save($website);
+        }
+
+        if($website->getId()){
+            /** @var \Magento\Store\Model\Group $group */
+            $group = $this->_groupFactory->create();
+            $group->setWebsiteId($website->getWebsiteId());
+            $group->setName('MP Store');
+            $group->setCode('mp_store');
+            $this->_groupResourceModel->save($group);
+        }
+
         $store = $this->_storeFactory->create();
         $store->load('mp_storeview');
         if (!$store->getId()) {
             $store->setCode('mp_storeview');
-            $store->setName(' MP Store View');
-            $store->setWebsiteId($this->_storeManager->getDefaultStoreView()->getWebsiteId());
-            $store->setGroupId($this->_storeManager->getDefaultStoreView()->getGroupId());
+            $store->setName('MP Store View');
+            $store->setWebsiteId($website->getWebsiteId());
+            $store->setGroupId($group->getGroupId());
             $store->setData('is_active', '1');
             $store->setSortOrder(0);
             try {
--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -7,44 +7,69 @@ use Magento\Framework\Setup\SchemaSetupInterface;
 use Magento\Framework\DB\Ddl\Table;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Store\Model\StoreFactory;
-use Magento\Store\Model\StoreManagerInterface;
 use Magento\Framework\Event\ManagerInterface;
 use Magento\Store\Model\ResourceModel\Store;
+use Magento\Store\Model\WebsiteFactory;
+use Magento\Store\Model\ResourceModel\Website;
+use Magento\Store\Model\GroupFactory;
+use Magento\Store\Model\ResourceModel\Group;
 
 class UpgradeSchema implements UpgradeSchemaInterface
 {
+    /**
+     * @var WebsiteFactory
+     */
+    protected $_websiteFactory;
+    /**
+     * @var GroupFactory
+     */
+    protected $_groupFactory;
+    /**
+     * @var Group
+     */
+    protected $_groupResourceModel;
     /**
      * @var StoreFactory
      */
     protected $_storeFactory;
     /**
-     * @var StoreManagerInterface
-     */
-    protected $_storeManager;
-    /**
      * @var ManagerInterface
      */
     protected $_eventManager;
+     /**
+     * @var Website
+     */
+    protected $_websiteResourceModel;
     /**
      * @var Store
      */
     protected $_storeResourceModel;
     /**
      * UpgradeSchema constructor.
-     * @param StoreFactory $storeFactory
-     * @param StoreManagerInterface $storeManager
+     * @param WebsiteFactory $WebsiteFactory
+     * @param Website $websiteResourceModel
+     * @param StoreFactory $StoreFactory
      * @param ManagerInterface $eventManager
+     * @param GroupFactory $GroupFactory
+     * @param Group $groupResourceModel
+     * @param Store $storeResourceModel
      */
     public function __construct(
+        WebsiteFactory $websiteFactory,
+        Website $websiteResourceModel,
         StoreFactory $storeFactory,
-        StoreManagerInterface $storeManager,
         ManagerInterface $eventManager,
+        GroupFactory $groupFactory,
+        Group $groupResourceModel,
         Store $storeResourceModel
     ) {
+        $this->_websiteFactory = $websiteFactory;
+        $this->_websiteResourceModel = $websiteResourceModel;
         $this->_storeFactory = $storeFactory;
-        $this->_storeManager = $storeManager;
         $this->_eventManager = $eventManager;
         $this->_storeResourceModel = $storeResourceModel;
+        $this->_groupFactory = $groupFactory;
+        $this->_groupResourceModel = $groupResourceModel;
     }
     /**
      * Upgrades DB schema for a module
@@ -100,13 +125,31 @@ class UpgradeSchema implements UpgradeSchemaInterface
                 ]
             );
 
+        /** @var \Magento\Store\Model\Website $website */
+        $website = $this->_websiteFactory->create();
+        $website->load('mp_web');
+        if(!$website->getId()){
+            $website->setCode('mp_web');
+            $website->setName('MP Website');
+            $this->_websiteResourceModel->save($website);
+        }
+
+        if($website->getId()){
+            /** @var \Magento\Store\Model\Group $group */
+            $group = $this->_groupFactory->create();
+            $group->setWebsiteId($website->getWebsiteId());
+            $group->setName('MP Store');
+            $group->setCode('mp_store');
+            $this->_groupResourceModel->save($group);
+        }
+
         $store = $this->_storeFactory->create();
         $store->load('mp_storeview');
         if (!$store->getId()) {
             $store->setCode('mp_storeview');
-            $store->setName(' MP Store View');
-            $store->setWebsiteId($this->_storeManager->getDefaultStoreView()->getWebsiteId());
-            $store->setGroupId($this->_storeManager->getDefaultStoreView()->getGroupId());
+            $store->setName('MP Store View');
+            $store->setWebsiteId($website->getWebsiteId());
+            $store->setGroupId($group->getGroupId());
             $store->setData('is_active', '1');
             $store->setSortOrder(0);
             try {
